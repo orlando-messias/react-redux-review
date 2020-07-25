8d6817385fcfd1320f865a348e809f3bdfb19c09
Orlando <programadorlando@gmail.com>
2020-07-25 19:46:04 -0300

    react-redux-review

---
+++ src/components/Home.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Table from './Table';
+import { connect } from 'react-redux';
+import { fetchPlanets } from '../actions';
+import FilterName from './FilterName';
+import FilterValues from './FilterValues';
+
+export class Home extends Component {
+
+  componentDidMount() {
+    const { fetchPlanets } = this.props;
+    fetchPlanets();
+    console.log(fetchPlanets())
+  }
+
+  render() {
+    const { isFetching } = this.props;
+    if (isFetching) return <h3>Loading...</h3>
+    return (
+      <div>
+        <h3>Strawars DataTable</h3>
+        <FilterName />
+        <FilterValues />
+        <Table />
+      </div>
+    )
+  }
+}
+
+const mapState = (state) => ({
+  isFetching: state.getPlanets.isFetching,
+})
+
+const mapDispatch = (dispatch) => ({
+  fetchPlanets: () => dispatch(fetchPlanets()),
+});
+
+export default connect(mapState, mapDispatch)(Home);
+
